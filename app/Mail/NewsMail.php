b2107de00de67a9d774e032ade7541e3e86e139e
Roman Marinichev <rmarinicev@gmail.com>
2022-12-30 18:10:32 +0300

    add email Queue

--- app/Mail/NewsMail.php
+++ app/Mail/NewsMail.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\News;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewsMail extends Mailable
+class NewsMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
--- app/Mail/VerificationMail.php
+++ app/Mail/VerificationMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class VerificationMail extends Mailable
+class VerificationMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
