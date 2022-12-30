82542d234af526808d199b5c716af5ae0e993255
Arsipa <senya.suvorov2@gmail.com>
2022-12-30 15:25:48 +0300

    finish without mailing

--- resources/views/inc/mailing.blade.php
+++ resources/views/inc/mailing.blade.php
@@ -1,9 +1,31 @@
 <section class="bg mailing">
     <div class="container">
         <h3 class="tac white">Подписаться на рассылку новостей</h3>
-        <form action="">
-            <input type="mail" placeholder="Ваш email*" required>
+        <form id="mailing" method="POST">
+            <input type="mail" name="email" placeholder="Ваш email*" required>
             <button class="btn" type="submit">Подписаться</button>
         </form>
     </div>
 </section>
+<script>
+    mailing.onsubmit = (e) => {
+        e.preventDefault();
+        let fd = new FormData(mailing);
+        fetch("/api/newsletter/subscription", {
+            method: 'POST',
+            body: fd
+        }).then(response => {
+            switch (response.status) {
+                case 200:
+                    alert("Вам на почту придет письмо с подтверждением!")
+                    break;
+                case 422:
+                    alert("Ошибка: Почта уже зарегистирована")
+                    break;
+                case 429:
+                    alert("Ошибка: Количество попыток превышено! Попробуйте снова через 30 минут.")
+                    break;
+            }
+        })
+    }
+</script>
