669e8c66f7f971193a4ae01b09b69ae36d509470
Arsipa <senya.suvorov2@gmail.com>
2022-12-30 21:21:42 +0300

    fix mailing message

--- resources/views/mailing-result.blade.php
+++ resources/views/mailing-result.blade.php
@@ -3,7 +3,24 @@
 
 <head>
     <title>Подтверждение почты</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-{{$message}}
+    <style>
+        section{
+            width: 100vw;
+            height: 100vh;
+            color: white;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            gap: 20px;
+            font-size: 30px;
+        }
+    </style>
+    <section class="bg">
+        {{$message}}
+        <a class="btn" href="{{route("news")}}">Вернуть к новостям</a>
+    </section>
 </body>
